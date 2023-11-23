9c03577dbbaad760f9f6ccc67bf9c554311c7972
surattinon <surattinon.basu@gmail.com>
2023-11-23 16:16:32 +0700

    config docker

---
+++ startlab.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Start container
+docker compose -f lab-ite231/docker-compose.yaml up -d
---
+++ stoplab.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Stop container
+docker compose -f lab-ite231/docker-compose.yaml down
