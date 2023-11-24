2e664195fdd437dd65afab0fd084cb899908809d
surattinon <surattinon.basu@gmail.com>
2023-11-24 11:54:51 +0700

    add bash file

---
+++ runlab.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Exec Container
+docker exec -it lab-ite231-lab-1 bash
--- stoplab.sh
+++ stoplab.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Push container
+docker compose -f lab-ite231/docker-compose.yaml push
+
 # Stop container
 docker compose -f lab-ite231/docker-compose.yaml down
